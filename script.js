0c82f99cd0f9972634aba61bb78980d5c8eb254f
Femil Mirza <81964761+femilmirza@users.noreply.github.com>
2021-10-02 17:48:56 +0530

    Update script.js with a responsive version

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function imgSlider(anything) {
     document.querySelector(".pepsi").src = anything;
   }
-  function changeBgColor(color) {
+function changeBgColor(color) {
     const sec = document.querySelector(".sec");
     sec.style.background = color;
   }
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
